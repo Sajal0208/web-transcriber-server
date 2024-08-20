c9a5b7a7ddcf9c9cd02f2370b42372f4d425a7d1
Sajal0208 <sajaldewangan@gmail.com>
2024-08-20 20:58:51 +0530

    Changed constants

---
+++ src/constant.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_MODEL = "tiny.en";
+
+export const MODELS_LIST = ["tiny.en", "base.en"];
+
+export const MODELS = ["ggml-tiny.en.bin", "ggml-base.en.bin"];
+
+export const MODEL_OBJECT = {
+  "tiny.en": "ggml-tiny.en.bin",
+  "base.en": "ggml-base.en.bin",
+};

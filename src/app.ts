5469a2d95200d75efd93e54c2bf49e261f55c41c
Sajal0208 <sajaldewangan@gmail.com>
2024-08-20 20:58:42 +0530

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
-import { json } from "body-parser";
+import bodyParser from "body-parser";
 import { errorHandler } from "./middlewares/errors";
 import transcribeRouter from "./route";
+import cors from "cors";
 
 const app = express();
 
-app.use(json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/api", transcribeRouter);
 app.get("/", (req, res) => {

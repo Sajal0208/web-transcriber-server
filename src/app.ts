a50dea7b069042fa865d0843424f35db14678569
Sajal0208 <sajaldewangan@gmail.com>
2024-08-21 04:10:30 +0530

    required .env

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,18 @@ import bodyParser from "body-parser";
 import { errorHandler } from "./middlewares/errors";
 import transcribeRouter from "./route";
 import cors from "cors";
+import agenda from "./agenda/agendaSingleton";
+import path from "path";
 
 const app = express();
 
+const envPath = path.join(__dirname, "..", ".env");
+
+require("dotenv").config({
+  path: envPath,
+});
+
+agenda.start();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 

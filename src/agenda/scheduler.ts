b8038a433055fb40b345df44e7019fcdb600022a
Sajal0208 <sajaldewangan@gmail.com>
2024-08-21 12:56:43 +0530

    Changed agenda deletion time

--- src/agenda/scheduler.ts
+++ src/agenda/scheduler.ts
@@ -3,6 +3,6 @@ import agenda from "./agendaSingleton";
 export const schedule = {
   deleteFileAfterExpire: async (data: any) => {
     console.log("Scheduling delete-file job");
-    await agenda.schedule("in 2 seconds", "delete-file", data);
+    await agenda.schedule("in 10 minutes", "delete-file", data);
   },
 };
